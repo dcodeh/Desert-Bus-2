3ac5997af0de9668602521bc9c46c4bacbeb7747
code1010 <code1010@rochester.rr.com>
2016-03-29 11:04:21 -0400

    Removed random tabs for beautiful nano viewing

--- src/Window.java
+++ src/Window.java
@@ -23,11 +23,11 @@ import javax.swing.KeyStroke;
 public class Window extends JPanel{
 
 	static Bus bus = new Bus();
-	
+
 	double conv = .62222222222222;
-	
+
 	boolean instantiatedImage = false;
-	
+
 	static boolean u = false;
 	static boolean d = false;
 	static boolean l = false;
@@ -35,19 +35,19 @@ public class Window extends JPanel{
 	Color desertFar = new Color(255, 239, 69);
 	Color sky = new Color(88, 211, 255);
 	Color desertClose = new Color(173, 159, 0);
-	
+
 	static int[] xRoadPts = {-700, 350, 900};
 	static int[] yRoadPts = {525, 100, 525};
-	
+
 	static int[] xMPHNeedlePts = {-40, 0, 0}; 
 	static int[] yMPHNeedlePts = {0, -3, 3}; 
-	
-	
+
+
 	static int angle;
-	
+
 	static Image busImage;
-	
-	
+
+
 	public static void main(String [] args) throws InterruptedException{
 		JFrame frame = new JFrame("Desert Bus 2");
 		Window game = new Window();
@@ -57,8 +57,8 @@ public class Window extends JPanel{
 		frame.setVisible(true);
 		frame.setResizable(false);
 		frame.setLocationRelativeTo(null);
-		
-		
+
+
 		AbstractAction rightArrow = new AbstractAction() {
 		    public void actionPerformed(ActionEvent e) {
 		        xRoadPts[0]-= bus.speed/5;
@@ -66,7 +66,7 @@ public class Window extends JPanel{
 		        xRoadPts[2]-=bus.speed/5;
 		    }
 		};
-		
+
 		AbstractAction leftArrow = new AbstractAction() {
 		    public void actionPerformed(ActionEvent e) {
 		    	xRoadPts[0]+=bus.speed/5;
@@ -74,34 +74,34 @@ public class Window extends JPanel{
 		        xRoadPts[2]+=bus.speed/5;
 		    }
 		};
-		
+
 		AbstractAction upArrowRelease = new AbstractAction() {
 		    public void actionPerformed(ActionEvent e) {
 		        u = false;
 		    }
 		};
-//		
+//
 		AbstractAction downArrowRelease = new AbstractAction() {
 		    public void actionPerformed(ActionEvent e) {
 		        d = false;
 		    }
 		};
-		
+
 		AbstractAction upArrow = new AbstractAction() {
 		    public void actionPerformed(ActionEvent e) {
 		        u = true;
 		    }
 		};
-		
+
 		AbstractAction downArrow = new AbstractAction() {
 		    public void actionPerformed(ActionEvent e) {
 		    	d = true;
 		    }
 		};
-		
+
 		InputMap im = game.getInputMap(JPanel.WHEN_IN_FOCUSED_WINDOW);
 	    ActionMap am = game.getActionMap();
-	    
+
 	    //Key Presses
 	    im.put(KeyStroke.getKeyStroke(KeyEvent.VK_RIGHT, 0, false), "RightArrow");
 	    im.put(KeyStroke.getKeyStroke(KeyEvent.VK_LEFT, 0, false), "LeftArrow");
@@ -112,14 +112,14 @@ public class Window extends JPanel{
 	    am.put("LeftArrow", leftArrow);
 	    am.put("UpArrow", upArrow);
 	    am.put("DownArrow", downArrow);
-	    
+
 	    //Key releases
 	    im.put(KeyStroke.getKeyStroke(KeyEvent.VK_UP, 0, true), "releaseUpArrow");
 	    im.put(KeyStroke.getKeyStroke(KeyEvent.VK_DOWN, 0, true), "releaseDownArrow");
-	    
+
 	    am.put("releaseUpArrow", upArrowRelease);
 	    am.put("releaseDownArrow", downArrowRelease);
-		
+
 		while(true){
 			//main loop
 			frame.repaint();
@@ -127,33 +127,27 @@ public class Window extends JPanel{
 			bus.speed = bus.calculateSpeed();
 			Thread.sleep(50);
 		}
-		
+
 	}
-	
 	@Override
 	public void paint(Graphics g){
 		super.paint(g);
 		Graphics2D gd = (Graphics2D) g;
 //		gd.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_OFF);
-				
-		
 //		AffineTransform current = gd.getTransform();
 //		AffineTransform to = AffineTransform.getRotateInstance(Math.toRadians(angle), 350, 263);		
-		
 		gd.setColor(desertFar);
 		gd.fillRect(0, 100, 700, 525);
-		
 		gd.setColor(sky);
 		gd.fillRect(0, 0, 700, 100);
-		
-		int[] newX = new int[3];		
+		int[] newX = new int[3];
 		newX[0] = xRoadPts[0] - 150;
 		newX[1] = xRoadPts[1];
 		newX[2] = xRoadPts[2] + 150;
-		
+
 		gd.setColor(desertClose);
 		gd.fillPolygon(newX, yRoadPts, yRoadPts.length);
-		
+
 		gd.setColor(Color.LIGHT_GRAY);
 //		gd.setTransform(to);
 		gd.fillPolygon(xRoadPts, yRoadPts, xRoadPts.length);
@@ -163,49 +157,49 @@ public class Window extends JPanel{
 		xLinePts[0] = xRoadPts[0] + 775;
 		xLinePts[1] = xRoadPts[1];
 		xLinePts[2] = xRoadPts[2] - 775;
-		
+
 		int[] yLinePts = {525, 100, 525};
 		gd.fillPolygon(xLinePts, yLinePts, xLinePts.length);
-		
-		
+
+
 		gd.setColor(Color.white);
 		gd.drawPolygon(xRoadPts, yRoadPts, xRoadPts.length);
-		
-		
+
+
 //		gd.drawString("GEAR " + String.valueOf(bus.currentGear), 100, 100);
 //		gd.drawString("RPM " + String.valueOf(bus.engineRPM), 100, 120);
 //		gd.drawString("SPEED " + String.valueOf(bus.speed), 100, 140);
-		
+
 		if(!instantiatedImage){
-		
+
 			try {
 				busImage = ImageIO.read(this.getClass().getResource("transparent db.png"));
 			} catch (IOException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
-			
+
 //			int newWidth = (int)(busImage.getWidth(null)*conv);
 //			int newHeight = (int)(busImage.getHeight(null) * conv);
-			
-			
+
+
 		}
-		
+
 		gd.drawImage(busImage, 0, 0, 700, 525, null);
 		drawRPM(gd);
 		drawMPH(gd);
-		
+
 		gd.setFont(new Font("Courier", 12, 12));
 		gd.setColor(Color.GREEN);
-		
+
 		gd.drawString(String.format("%.1fmi", bus.totalMiles), 170, 350);
-		
+
 	}
-	
+
 	public void drawRPM(Graphics2D gd){
-		
+
 		gd.setColor(Color.black);
-		
+
 		AffineTransform current = gd.getTransform();
 		double theta = Math.toRadians(-45 + (.0675*bus.engineRPM));
 		AffineTransform newT = AffineTransform.getRotateInstance(theta, 95, 355);
@@ -214,11 +208,11 @@ public class Window extends JPanel{
 		gd.fillPolygon(xMPHNeedlePts, yMPHNeedlePts, xMPHNeedlePts.length);
 		gd.setTransform(current);
 	}
-	
+
 	public void drawMPH(Graphics2D gd){
-		
+
 		gd.setColor(Color.black);
-		
+
 		AffineTransform current = gd.getTransform();
 		double theta = Math.toRadians(-45 + (3.375*bus.speed));
 		AffineTransform newT = AffineTransform.getRotateInstance(theta, 315, 355);
@@ -226,7 +220,7 @@ public class Window extends JPanel{
 		gd.translate(315, 355);
 		gd.fillPolygon(xMPHNeedlePts, yMPHNeedlePts, xMPHNeedlePts.length);
 		gd.setTransform(current);
-		
+
 	}
-	
+
 }
